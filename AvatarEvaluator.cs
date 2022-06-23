3711bfc8fbf37c51013e0f3b87d020bb8e91e779
Tayou <31988415+TayouVR@users.noreply.github.com>
2022-06-23 15:57:42 +0200

    Make GetMaterials() consider EditorOnly Tag when evaluating materials

--- AvatarEvaluator.cs
+++ AvatarEvaluator.cs
@@ -261,8 +261,8 @@ namespace Thry.AvatarHelpers {
 
         public static IEnumerable<Material>[] GetMaterials(GameObject avatar)
         {
-            List<Material> materialsActive = avatar.GetComponentsInChildren<Renderer>(false).SelectMany(r => r.sharedMaterials).ToList();
-            List<Material> materialsAll = avatar.GetComponentsInChildren<Renderer>(true).SelectMany(r => r.sharedMaterials).ToList();
+            List<Material> materialsActive = avatar.GetComponentsInChildren<Renderer>(false).Where(r => r.gameObject.tag != "EditorOnly").SelectMany(r => r.sharedMaterials).ToList();
+            List<Material> materialsAll = avatar.GetComponentsInChildren<Renderer>(true).Where(r => r.gameObject.tag != "EditorOnly").SelectMany(r => r.sharedMaterials).ToList();
 #if VRC_SDK_VRCSDK3 && !UDON
             //animation materials
             VRCAvatarDescriptor descriptor = avatar.GetComponent<VRCAvatarDescriptor>();
